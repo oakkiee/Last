d46bcff4361729092f41940cff6af459367ec9d2
YOUR NAME <YOUR@EMAIL.com>
2024-12-01 14:13:22 +0700

    install react js and tailwind css and create the first template

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-estate-94a97.firebaseapp.com",
+  projectId: "mern-estate-94a97",
+  storageBucket: "mern-estate-94a97.firebasestorage.app",
+  messagingSenderId: "626861858319",
+  appId: "1:626861858319:web:595d935ecd8efb871a8d7a"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
